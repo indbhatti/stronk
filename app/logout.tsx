c33a2ac614a6c300092eb7c80b5fda057bbdbdb6
walruii <walruiix@gmail.com>
2024-11-13 22:48:59 +0530

    fixed the linting eror

--- app/logout.tsx
+++ app/logout.tsx
@@ -8,6 +8,7 @@ import {
   removeRefreshTokenServer,
   removeTokenServer,
 } from "@/serverActions/serverCookieUtils";
+import Link from "next/link";
 
 export default function Logout() {
   const logout = async () => {
@@ -18,12 +19,12 @@ export default function Logout() {
   };
   return (
     <div className="hidden sm:flex" onClick={logout}>
-      <a
+      <Link
         className="rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600"
         href="/"
       >
         Logout
-      </a>
+      </Link>
     </div>
   );
 }
