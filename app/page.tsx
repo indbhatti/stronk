df229aeed6d6917dd322af3d375d3274f65982c2
walruii <walruiix@gmail.com>
2024-11-13 22:47:03 +0530

    quick fix for it

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,12 @@
-import { getDecodedServerToken } from "@/serverActions/serverCookieUtils";
+// import { getDecodedServerToken } from "@/serverActions/serverCookieUtils";
 import Banner from "./banner";
 import Header from "./header";
-import gamer from "./logout";
-import Logout from "./logout";
 
 export default async function Home() {
-  const decodedToken = await getDecodedServerToken();
-  const username = decodedToken ? decodedToken.username : null;
-  const email = decodedToken ? decodedToken.email : null;
-  const accessLevel = decodedToken ? decodedToken.accessLevel : null;
+  // const decodedToken = await getDecodedServerToken();
+  // const username = decodedToken ? decodedToken.username : null;
+  // const email = decodedToken ? decodedToken.email : null;
+  // const accessLevel = decodedToken ? decodedToken.accessLevel : null;
 
   return (
     <>
