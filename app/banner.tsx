48a4738ce38a9d06d2822f2851df98ddb150f39f
walruii <walruiix@gmail.com>
2024-11-14 12:30:51 +0530

    changed favicon, changes a tags to Link tags and changes the title

--- app/banner.tsx
+++ app/banner.tsx
@@ -1,33 +1,35 @@
+import Link from "next/link";
+
 export default function Banner() {
   return (
     <section className="bg-gray-900 text-white">
       {" "}
       <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
         <div className="mx-auto max-w-3xl text-center">
-          <h1 className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl">
-            Understand User Flow.
-            <span className="sm:block"> Increase Conversion. </span>
+          <h1 className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl pb-3">
+            Understand Muscle Growth.
+            <span className="sm:block"> Increase Hypertrophy. </span>
           </h1>
 
           <p className="mx-auto mt-4 max-w-xl sm:text-xl/relaxed">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt
-            illo tenetur fuga ducimus numquam ea!
+            Join us on an adventure to understand muscle growth and increase
+            hypertrophy.
           </p>
 
           <div className="mt-8 flex flex-wrap justify-center gap-4">
-            <a
+            <Link
               className="block w-full rounded border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-white focus:outline-none focus:ring active:text-opacity-75 sm:w-auto"
-              href="#"
+              href="/auth/register"
             >
               Get Started
-            </a>
+            </Link>
 
-            <a
+            <Link
               className="block w-full rounded border border-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring active:bg-blue-500 sm:w-auto"
-              href="#"
+              href="/"
             >
               Learn More
-            </a>
+            </Link>
           </div>
         </div>
       </div>
--- app/header.tsx
+++ app/header.tsx
@@ -1,6 +1,8 @@
 import { getDecodedServerToken } from "@/serverActions/serverCookieUtils";
 import Navbar from "./navbar";
 import Logout from "./logout";
+import Link from "next/link";
+import Image from "next/image";
 
 export default async function Header() {
   const decodedToken = await getDecodedServerToken();
@@ -8,42 +10,40 @@ export default async function Header() {
     <header className="bg-gray-900 px-10">
       <div className="flex h-16 items-center justify-between">
         <div className="flex-1 md:flex md:items-center md:gap-12">
-          <a className="block text-teal-600" href="#">
+          <Link className=" text-teal-600 flex items-center" href="/">
             <span className="sr-only">Home</span>
-            {/* <img></img> */}
-          </a>
+            <Image
+              src="/fitness.png"
+              alt="Fitness Icon"
+              width={50}
+              height={50}
+            />
+            <h1 className="text-xl ml-3">Stronk</h1>
+          </Link>
         </div>
 
         <div className="md:flex md:items-center md:gap-12">
           <Navbar />
           <div className="flex items-center gap-4">
             {decodedToken == null ? (
-              <div className="sm:flex sm:gap-4">
-                <a
-                  className="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow"
+              <div>
+                <Link
+                  className="rounded-md bg-teal-600 px-5 py-2.5 mx-2 text-sm font-medium text-white shadow"
                   href="/auth/login"
                 >
                   Login
-                </a>
+                </Link>
 
-                <div className="hidden sm:flex">
-                  <a
-                    className="rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600"
-                    href="/auth/register"
-                  >
-                    Register
-                  </a>
-                </div>
+                <Link
+                  className="rounded-md bg-gray-100 px-5 py-2.5 ml-2 text-sm font-medium text-teal-600"
+                  href="/auth/register"
+                >
+                  Register
+                </Link>
               </div>
             ) : (
               <Logout />
             )}
-
-            <div className="block md:hidden">
-              <button className="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75">
-                {/* <img></img> */}
-              </button>
-            </div>
           </div>
         </div>
       </div>
